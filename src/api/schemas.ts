1cc8007f1c29fc24cdd198bc19dc9aad6de27f73
Henry Shen <83993302+henryshen-le@users.noreply.github.com>
2021-11-13 14:03:32 +1100

    add images

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -7,6 +7,17 @@ const fetchLinkSchema = {
     // body: s({ id: 'uuid', name: 'string', age: 'number' })
 }
 
+const createLinkSchema = {
+    params: s.objectWithOnly({
+        id: s.string()
+    }),
+    body: s.objectWithOnly({
+        type: s.string()
+
+    })
+    // body: s({ id: 'uuid', name: 'string', age: 'number' })
+}
+
 export {
     fetchLinkSchema
 }
